78fc796d673e0b8621cbb89cbf0a1f9778ae5a87
Artur Ishmuratov <ru_man1987@icloud.com>
2022-05-22 11:41:59 +0300

    concatinate text from files

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 //Содержимое директории
 const fs = require('fs');
 const path = require('path');
-const dir = './';
+let dir = './';
+//  fs.readdir(dir, (err, data) => {
+//       console.log(data)
+//      console.log(path.extname(data[1]))// type file
+//  })
+
+
 fs.readdir(dir, (err, data) => {
-    console.log(data)
-    console.log(path.extname(data[1]))// type file
-    // data.forEach(file => {
-    //     fs.readFile('file','utf-8', (err, data) => {
-    //              console.log(data);
-    //          })
-    // })
-   
+     // console.log(data);
+      const doubles = data.map((num) =>
+            fs.readFile(dir + num, 'utf-8', (err, data) => {
+                  if (path.extname(num) == '.txt') {
+                        console.log(data)
+                  };
+            })
+      );
+
 })
+
+
+
+// let file = 'abc.txt';
+
+//  fs.readFile( file,'utf-8', (err, data) =>{
+//   console.log(data);
+//  })
+
+
+
+
 //Чтение файла
-fs.readFile('abc.txt','utf-8', (err, data) =>{
-    console.log(data);
-})
+// fs.readFile('abc.txt','utf-8', (err, data) =>{
+//     console.log(data);
+// })
